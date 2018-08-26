11b3d96d91a39b6e8be6617ebb63fd8a414ba02c
Carley Tripp <carley.tripp@outlook.com>
2018-08-26 02:53:43 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -26,5 +26,6 @@ class PostsController < ApplicationController
 	def update
 		@post = Post.find(params[:id])
 		@post.update(params[:title], params[:description])
+		redirect_to post_path(@post)
 	end
 end
